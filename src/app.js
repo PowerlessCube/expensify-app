1aae70e1d4a83e8d71a01e4dbf492984d81de309
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-14 22:52:54 +0000

    React-Router 101 - Challenge complete.

--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// 1 instance of Browserrouter to create new router and use route for everysingle page. path and what we want to do when user visits that page.
 import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
 const ExpenseDashBoardPage = () => (
     <div>
-        This is from my add expense component
+        This is from my dashboard component
     </div>
 );
 
 const AddExpensePage = () => (
     <div>
-        This is from my dashboard component
+        This is from my add expense component    
     </div>
 );
 
-/*
-Challenge 
-edit EditExpensePage
-help HelpPage
-*/
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component.
+    </div>
+);
 
-// Route Requires 2 props - path and component.
-// exact says it will only look for the exact route.
+const helpPage = () => (
+    <div>
+        This is from help page.
+    </div>
+);
+
 const routes = (
     <BrowserRouter> 
         <div>
             <Route path='/' component={ExpenseDashBoardPage} exact={true}/>
             <Route path='/create' component={AddExpensePage}/>
+            <Route path='/edit-expense-page' component={EditExpensePage}/>
+            <Route path='/help-page' component={helpPage}/>
         </div>
     </BrowserRouter>
 );
 
 ReactDOM.render(routes, document.getElementById('app'));
-
-/* 
-React Router 
-https://reacttraining.com/react-router/
-*/

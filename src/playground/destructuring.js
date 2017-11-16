742c1f4af7f8a246d6ee5109b3efa0c7f85e2c84
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-16 06:32:12 +0000

    ES6 Destructuring - Challenge issued

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,24 +1,36 @@
-const person = {
-    name: 'Alistair',
-    age: 29,
-    location: {
-        city: 'Edinburgh',
-        temp: 11
+// const person = {
+//     name: 'Alistair',
+//     age: 29,
+//     location: {
+//         city: 'Edinburgh',
+//         temp: 11
+//     }
+// };
+// // Simple object destructing
+
+// // Default value
+// const { name: firstName = 'Anonymous', age } = person;
+// // const name = person.name;
+// // const age = person.age;
+
+// console.log(`${firstName} is ${age}.`);
+// // console.log(`${person.name} is ${person.age}.`);
+
+// // Re-naming varibales
+// const {city, temp: temperature} = person.location;
+
+// if (city && temperature) {
+//     console.log(`it's ${temperature} deg in ${city}.`);
+// }
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    published: {
+        name: 'Penguin'
     }
 };
-// Simple object destructing
 
-// Default value
-const { name: firstName = 'Anonymous', age } = person;
-// const name = person.name;
-// const age = person.age;
+//Challenge - use destructuring to cl the below.
 
-console.log(`${firstName} is ${age}.`);
-// console.log(`${person.name} is ${person.age}.`);
-
-// Re-naming varibales
-const {city, temp: temperature} = person.location;
-
-if (city && temperature) {
-    console.log(`it's ${temperature} deg in ${city}.`);
-}
+console.log(publisherName); // Penguin default = self-published

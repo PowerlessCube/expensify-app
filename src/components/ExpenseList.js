ae8952f98b975d6b3de3aa40cf0fb42d5b2f1591
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-17 23:31:20 +0000

    Rendering Indivdual Expenses - Challenge completed added ExpenseListItem component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,26 +1,20 @@
 import React from 'react';
-// We call connect
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem'
 
-// Regular unconnected component
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.expenses.length}
+        {props.expenses.map((expense) => {
+            return <ExpenseListItem key={expense.id} {...expense} />
+        })}        
     </div>
 );
 
-// We define the things that we want to get form the store
-// Some functions
 const mapStateToProps = (state) => {
     return {
         expenses: state.expenses,
-        filters: state.filters
     };
 }
 
-// Connection to the store
-// We get functions back from this so we have to call it. Hence the 
-
-// and we define the component we want to connect to the store
 export default connect(mapStateToProps)(ExpenseList);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,3 +1,14 @@
-// Export a stateless functional component
+import React from 'react';
+import { connect } from 'react-redux';
 
+// Export a stateless functional component
+const ExpenseListItem = (props) => (
+    <div>
+        <p>{props.description}</p>
+        <p>{props.amount}</p>
+        <p>{props.createdAt}</p>
+    </div>
+);
+
+export default ExpenseListItem;
 // description, amount, createdAt

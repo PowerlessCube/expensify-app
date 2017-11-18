32febc0a9a7dcb6707af7bb35d1f5b5e36301ac3
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-18 23:40:00 +0000

    Testing Filters Action Generators - Challenge issued added tests for all filter action outcomes.

---
+++ src/tests/filters/filters.test.js
@@ -0,0 +1,56 @@
+import { 
+    setStartDate, 
+    setEndDate, 
+    setTextFilter, 
+    sortByDate, 
+    sortByAmount 
+} from '../../actions/filters';
+import moment from 'moment';
+
+test('should generate set start date action object', () => {
+    const action = setStartDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    })
+});
+
+test('should generate set end date action object', () => {
+    const action = setEndDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    })  
+});
+
+test('should generare set text filter action object with given value', () => {
+    const action = setTextFilter('test');
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'test'
+    })
+});
+
+test('should generare set text filter action object with default value', () => {
+    const action = setTextFilter();
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    })
+});
+
+test('should generate sort by date action object', () => {
+    const action = sortByDate();
+    expect(action).toEqual({
+        type: "SORT_BY_DATE",
+        date: "date"
+    })
+});
+
+test('should generate sort by amount action object', () => {
+    const action = sortByAmount();
+    expect(action).toEqual({
+        type: "SORT_BY_AMOUNT",
+        amount: "amount"
+    })
+});

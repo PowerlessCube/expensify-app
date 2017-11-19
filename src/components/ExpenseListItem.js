db2d5797b17e75f6e0667bdcd43e46e4b3ee9f39
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-19 23:07:08 +0000

    testing expenses reducer - Challenge complete, added test cases for all routes and seperated out sample data into fixtures folder.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, note }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>{amount} - {createdAt} - {note}</p>
     </div>
 );
 
---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,76 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+import moment from 'moment';
+
+test('Should set default state', () => {
+    const state = expensesReducer(undefined, {  type: '@@INIT' })
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]])
+});
+
+test('should not remove expense if id is not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses)
+});
+
+test('should add an expense', () => {
+    const newExpense = {
+        id: '4',
+        description: 'take away',
+        note: 'yum tum thai',
+        amount: 5500,
+        createdAt: moment(0).add(8, 'days').valueOf()
+    }
+    const action = {
+        type: 'ADD_EXPENSE',
+        newExpense
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[1], expenses[2], expenses[3]])
+});
+
+test('should edit an expense', () => {
+    const updates = {
+        id: '2',
+        description: 'Rent',
+        note: 'New Note added',
+        amount: 109500,
+        createdAt: moment(0).subtract(4, 'days').valueOf()
+    }
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: updates.id,
+        updates
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], updates, expenses[2]])
+});
+
+test('should not edit expense if expense not found', () => {
+    const updates = {
+        id: '4',
+        description: 'Rent',
+        note: 'New Note added',
+        amount: 109500,
+        createdAt: moment(0).subtract(4, 'days').valueOf()
+    }
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: updates.id,
+        updates
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses)
+});

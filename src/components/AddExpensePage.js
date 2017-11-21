6b5abf9051ba6c81aebb39cacbc369dbe64dff30
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-21 22:51:05 +0000

    Testing AddExpensePage - Refactored AddExpensePage component and wrote tests utilising beforeEach jest command.

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,28 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm 
-            onSubmit={(expense) => {
-                props.dispatch(addExpense(expense));
-                props.history.push('/')
-            }}
-        />    
-    </div>
-);
+export class AddExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.onSubmit(expense);
+        this.props.history.push('/');
+    };
 
-export default connect()(AddExpensePage);
+    render() {
+        return (
+            <div>
+                <h1>Add Expense</h1>
+                <ExpenseForm 
+                   onSubmit={this.onSubmit}
+                />    
+            </div>
+        );
+    }
+}
+
+// https://github.com/reactjs/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
+// way to return your dispatch
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (expense) => dispatch(addExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);

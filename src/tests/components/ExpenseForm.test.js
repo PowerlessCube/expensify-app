2c6a12dfd5f4cd4555b5b8ce74138474a92b1677
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-21 20:40:55 +0000

    Mocking Libraries with Jest - Challenge completed, tests mocked.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
 
 test('should render ExpenseForm correctly', () => {
     const wrapper = shallow(<ExpenseForm />);
     expect(wrapper).toMatchSnapshot();
 });
 
-// Should render ExpenseForm with expense data
+test('Should render ExpenseForm correctly with expense data', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[1]}/>);
+    expect(wrapper).toMatchSnapshot();
+});

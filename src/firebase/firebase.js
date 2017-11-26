79700dc3b11e63ebb4d1481594cbdadbcc12fa1c
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-26 23:10:55 +0000

    Removing Data from Firebase - Challenge issued and completed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,33 +11,25 @@ const config = {
 
 firebase.initializeApp(config);
 
-const database = firebase.database()
-// https://firebase.google.com/docs/reference/js/firebase.database.Reference#set => docs for set()
-database.ref().set({
-    name: 'Alistair Mackay',
-    age: 29,
-    isMarried: true,
-    location: {
-        city: 'Edinburgh',
-        country: 'Scotland'
-    },
-}).then(() => {
-    console.log('Data got saved');
-}).catch((e) => {
-    console.log('this failed', e);
-});;
+const database = firebase.database();
+// database.ref().set({
+//     name: 'Alistair Mackay',
+//     age: 29,
+//     isMarried: true,
+//     location: {
+//         city: 'Edinburgh',
+//         country: 'Scotland'
+//     },
+// }).then(() => {
+//     console.log('Data got saved');
+// }).catch((e) => {
+//     console.log('this failed', e);
+// });
 
-// Challenge
-// setup then and catch
-// makesure catch actually works
-// switch rules to be open
-// make sure that then runs.
-
-database.ref('attributes').set({
-    height: 178,
-    weight: 95
-}).then(() => {
-    console.log('Data has changed!');
-}).catch(() => {
-    console.log('Something done goofed!');
-});
+database.ref('isMarried').remove()
+    .then(() => {
+        console.log('Data removed successfully');
+    })
+    .catch((e) => {
+        console.log('Remove Data done goofed! ', e);
+    })

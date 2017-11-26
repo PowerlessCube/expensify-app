a18ee51c898cc67b6e934016296b5148e1c19991
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-26 23:18:11 +0000

    Removing Data - completed section

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,24 +12,27 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
-// database.ref().set({
-//     name: 'Alistair Mackay',
-//     age: 29,
-//     isMarried: true,
-//     location: {
-//         city: 'Edinburgh',
-//         country: 'Scotland'
-//     },
-// }).then(() => {
-//     console.log('Data got saved');
-// }).catch((e) => {
-//     console.log('this failed', e);
-// });
+database.ref().set({
+    name: 'Alistair Mackay',
+    age: 29,
+    isMarried: true,
+    location: {
+        city: 'Edinburgh',
+        country: 'Scotland'
+    },
+}).then(() => {
+    console.log('Data got saved');
+}).catch((e) => {
+    console.log('this failed', e);
+});
 
-database.ref('isMarried').remove()
-    .then(() => {
-        console.log('Data removed successfully');
-    })
-    .catch((e) => {
-        console.log('Remove Data done goofed! ', e);
-    })
+database.ref('isMarried').set(null);
+
+// database.ref('isMarried')
+//     .remove()
+//     .then(() => {
+//         console.log('Data removed successfully');
+//     })
+//     .catch((e) => {
+//         console.log('Remove Data done goofed! ', e);
+//     })

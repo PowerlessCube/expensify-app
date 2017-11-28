0cff49bbe9e43ba1ae13e73068c184173a9d110c
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-28 00:15:31 +0000

    Fetching Expenses: Part I - Challenge completed, added set state action and reducer with tests

--- src/tests/actions/expense.test.js
+++ src/tests/actions/expense.test.js
@@ -1,10 +1,18 @@
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk])
+
+beforeEach((done) => {
+    const expensesData = {}
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt}
+    })
+    database.ref('expenses').set(expensesData).then(() => done())
+})
 
 test('Should setup remove expense action object', () => {
     const action = removeExpense({ id: '123abc' });
@@ -90,4 +98,12 @@ test('Should add expense with defaults to database and store', (done) => {
         expect(snapshot.val()).toEqual(expenseDefault);
         done();
     });
+});
+
+test('Should setup set expense action object with data', () => {
+    const action = setExpenses(expenses);
+    expect(action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    })
 });
